513b2f60aca398fe455ba6df1318ea5c1f756bf3
Tim Wright <tim.wright@freshtilledsoil.com>
2013-08-26 08:21:08 -0400

    JS cleaning

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-    $access_token = '[your token here]';
-    teams;
+    var $access_token = '[your token here]';
+    var teams;
   
     $.getJSON("http://api.cbssports.com/fantasy/league/teams?access_token=" + $access_token + "&response" function(data) {
 
